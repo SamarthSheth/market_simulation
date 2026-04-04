f12c389c91e4066f9b6de4edc6a8ac0c87ab5a25
Samarth <samarth.sheth@hotmail.com>
2026-04-04 13:01:55 -0500

    added abm price process

---
+++ src/price_process.py
@@ -0,0 +1,32 @@
+
+
+from typing import List
+
+import numpy as np
+from order_book import OrderBook, Trade
+
+
+class PriceProcess:
+    def __init__(self, curr_price, curr_time, mu, sigma, rng) -> None:
+        self.curr_price = curr_price
+        self.curr_time = curr_time
+        #self.config = config
+        self.mu = mu
+        self.sigma = sigma
+        self.rng = rng
+        self.history: List[tuple[float, float]] = [(curr_time, curr_price)]
+        
+    def step(self, dt: float) -> float:
+        """Advance the true price by one time step. Returns new price."""
+        # For simplicity, we model the price as am arithmetic Brownian motion with drift
+        dW = self.rng.normal(0, np.sqrt(dt))
+        dS = self.mu * dt + self.sigma * dW
+        self.curr_price += dS
+        self.curr_time += dt
+        self.history.append((self.curr_time, self.curr_price))
+        return self.curr_price
+    
+    def get_current_price(self) -> float:
+        return self.curr_price
+        
+        
